96e622616a52a05987cc29ea502330c5facbb007
Nezumi-Quasar <Nezumi-Quasar@users.noreply.github.com>
2023-01-19 06:27:25 +0000

    script updated

--- vmss/InstallDockerComposePluginOnUbuntu.sh
+++ vmss/InstallDockerComposePluginOnUbuntu.sh
@@ -11,5 +11,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
-apt-get install -y  docker-compose-plugin
+
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+apt-get update -y 
+
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
